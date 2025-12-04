9f1c1671330136db3da0130bb92fca6228706da4
Murz4 <cool.murz@gmail.com>
2025-12-04 19:13:08 +0100

    sync

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,12 +1,12 @@
 import { Provider } from 'react-redux';
 import { store } from '@common/store/store';
+import { AuthPage } from '@pages/authPage/AuthPage';
 import { setupInterceptors } from 'src/api/instances';
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import { AuthPage } from '../pages/AuthPage/AuthPage';
-import { CalendarPage } from '../pages/CalendarPage/CalendarPage';
-import { TasksPage } from '../pages/TasksPage/TasksPage';
-import { Layout } from '../pages/Layout/Layout';
-import { MainPage } from '../pages/MainPage/MainPage';
+import { MainPage } from '@pages/MainPage/MainPage';
+import { Layout } from '@pages/Layout/Layout';
+import { CalendarPage } from '@pages/CalendarPage/CalendarPage';
+import { TasksPage } from '@pages/TasksPage/TasksPage';
 
 setupInterceptors(store);
 
